39b45c167c6e3cfc278ce0a5f7f01cf1652a50c8
chalatz <chralatz@gmail.com>
2016-03-01 12:24:37 +0000

    added forms partials

--- resources/views/sites/forms/edit.blade.php
+++ resources/views/sites/forms/edit.blade.php
@@ -12,44 +12,7 @@
 
                   {!! Form::model($site, ['method' => 'PUT', 'route' => ['sites.update', $site->id], 'class' => 'form-horizontal', 'role' => 'form']) !!}
 
-                    <div class="col-md-12 form-group{{ $errors->has('url') ? ' has-error' : '' }}">
-                        {{ Form::label('url', 'Διεύθυνση URL') }}
-                        {{ Form::url('url', null, ['class' => 'form-control', 'id' => 'url']) }}
-
-                        @if ($errors->has('url'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('url') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-
-                    <div class="col-md-12 form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-                        {{ Form::label('title', 'Ονομασία') }}
-                        {{ Form::text('title', null, ['class' => 'form-control', 'id' => 'title']) }}
-
-                        @if ($errors->has('title'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('title') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-
-                    <div class="col-md-12 form-group{{ $errors->has('cat_id') ? ' has-error' : '' }}">
-                        {{ Form::label('cat_id', 'Κατηγορία') }}
-                        {{ Form::select('cat_id', $categories::all(), null, ['class' => 'form-control', 'id' => 'cat_id']) }}
-
-                        @if ($errors->has('cat_id'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('cat_id') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-12">
-                            {{ Form::button('Αποθήκευση', ['type' => 'submit', 'class' => 'btn btn-primary btn-block btn-lg']) }}
-                        </div>
-                    </div>
+                    @include('sites.forms.partials.sites_form')
 
                   {!! Form::close() !!}
 
