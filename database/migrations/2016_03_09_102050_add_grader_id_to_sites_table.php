8b6b2b9d988d2f2d964f37792e14cfea50e71831
chalatz <chralatz@gmail.com>
2016-03-09 10:24:32 +0000

    added grader_id to sites table

---
+++ database/migrations/2016_03_09_102050_add_grader_id_to_sites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGraderIdToSitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sites', function (Blueprint $table) {
+            $table->integer('grader_id')->unsigned()->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sites', function (Blueprint $table) {
+            //
+        });
+    }
+}
