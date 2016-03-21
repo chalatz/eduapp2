ab13d155a6936ca9a14173694291b33278a2b631
chalatz <chralatz@gmail.com>
2016-03-21 09:11:25 +0000

    minor changes

--- resources/views/pages/choose_grader_type.blade.php
+++ resources/views/pages/choose_grader_type.blade.php
@@ -2,18 +2,27 @@
 
 @section('content')
 <div class="container">
+
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
 
-            <a href="{{ route('suggest_other_grader') }}" type="button" class="btn btn-info btn-lg btn-block">
-                Προτείνω Άλλον
-            </a>
-
             <a href="{{ route('graders.create') }}" type="button" class="btn btn-success btn-lg btn-block">
-                Προτείνω Εμένα
+                Προτείνω τον εαυτό μου
             </a>
 
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+
+            <a href="{{ route('suggest_other_grader') }}" type="button" class="btn btn-info btn-lg btn-block">
+                Προτείνω κάποιον άλλον
+            </a>
+
+        </div>
+    </div>
+
 </div>
+
 @endsection
