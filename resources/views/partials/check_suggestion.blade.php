523e7fb71fa45bcafdbdd060225d911dcb368faa
chalatz <chralatz@gmail.com>
2016-04-08 11:17:56 +0000

    added acceptance link to user front page

--- resources/views/partials/check_suggestion.blade.php
+++ resources/views/partials/check_suggestion.blade.php
@@ -2,9 +2,18 @@
 
   @if(Auth::user()->hasSuggestionToRespondTo())
 
-    <blockquote class="lead">
-        Ο χρήστης {{ Auth::user()->hasSuggestionToRespondTo()->suggestor_name }} με το email {{ Auth::user()->hasSuggestionToRespondTo()->suggestor_email }}, σας έχει προτείνει ως Αξιολογητή Α.
-    </blockquote>
+    <div class="row">
+      <blockquote class="lead">
+          Ο χρήστης {{ Auth::user()->hasSuggestionToRespondTo()->suggestor_name }} με το email {{ Auth::user()->hasSuggestionToRespondTo()->suggestor_email }}, σας έχει προτείνει ως Αξιολογητή Α.
+      </blockquote>
+    </div>
+
+    <div class="row">
+        <a href="{{ route('user.suggest', Auth::user()-> hasSuggestionToRespondTo()->unique_string) }}" type="button" class="btn btn-primary btn-lg btn-block">
+            Αποδοχή ή όχι της πρόκλησης
+        </a>
+    </div>
+
 
   @endif
 
