adee662811cdd275ccbfcba348bc9d637050124e
chalatz <chralatz@gmail.com>
2016-10-25 11:01:30 +0300

    Added a test log task

---
+++ app/Console/Commands/LogDemo.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use Log;
+
+use Carbon\Carbon;
+
+class LogDemo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'log:demo';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Testing testing';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Log::info('I was here @ ' . Carbon::now());
+    }
+}
