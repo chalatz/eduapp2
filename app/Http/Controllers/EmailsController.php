984ba22f284fa3e952fab70034d7200c076d80fb
chalatz <chralatz@gmail.com>
2016-12-06 20:57:08 +0200

    Fixed some email text

--- app/Http/Controllers/EmailsController.php
+++ app/Http/Controllers/EmailsController.php
@@ -26,15 +26,15 @@ class EmailsController extends Controller
 
                 $graders = DB::table('past_graders_b')->get();
 
-                $from = 201;
-                $to = 250;
+                $from = 1;
+                $to = 1;
 
                 foreach($graders as $grader){
                     if($grader->id >= $from && $grader->id <= $to ){
                         
                         Mail::send('emails.send_to_past_graders', ['grader' => $grader], function ($message) use ($grader) {
                         
-                            $message->to($grader->email, $grader->email)->subject('Πρόσκληση για Αξιολογητής Β Επιπέδου 8ου ΔΕΕΙ');
+                            $message->to($grader->email, $grader->email)->subject('ΟΡΘΗ ΕΠΑΝΑΛΗΨΗ ως προς ΗΜΕΡΟΜΗΝΙΑ - Πρόσκληση για Αξιολογητής Β Επιπέδου 8ου ΔΕΕΙ');
 
                         });
 
