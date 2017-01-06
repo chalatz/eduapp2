1cdb3cec8e78668f947f6414363c1d42da115c8f
chalatz <chralatz@gmail.com>
2017-01-06 16:04:01 +0200

    Corrected an email sender

--- app/Http/Controllers/EmailsController.php
+++ app/Http/Controllers/EmailsController.php
@@ -61,16 +61,18 @@ class EmailsController extends Controller
 
             $contest_index = Config::first()->index;
 
-            $data = [
-                'grader' => $grader,
-                'contest_index' => $contest_index,
-            ];
-
             $i = 1;
 
             foreach($graders as $grader){
-                if(!$grader->user->hasRole('member') || $grader->user->hasRole('ninja')){
+                if(!$grader->user->hasRole('member')){
                     if($grader->user->hasRole('grader_a') && !$grader->hasSite()){
+
+                        $data = [];
+
+                        $data = [
+                            'grader' => $grader,
+                            'contest_index' => $contest_index,
+                        ];
 
                         Mail::send('emails.send_to_graders_a_without_sites', $data, function ($message) use ($data) {                        
                             $message->to($data['grader']->user->email, $data['grader']->user->email)->subject('Σχετικά με την Υποψηφιότητά σας στον ' .  $data['contest_index'] .'ο ΔΕΕΙ');
@@ -78,6 +80,9 @@ class EmailsController extends Controller
 
                         echo $i . '. ' . $grader->id . '<br>';
                         $i++;
+
+                        unset($data);
+
                     }
                 }
             }
