47a5f7c8e883c88fdf591dc05ff6db7a86aed102
chalatz <chralatz@gmail.com>
2017-02-10 00:47:32 +0200

    changed index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,7 +11,13 @@
 
                 @include('pages.partials.info')
 
-                @include('pages.partials.start_gradings')
+                @if(App\Config::first()->phase_a_gradings && Auth::user()->grader && Auth::user()->grader->has_to_grade_a())
+                    <p>
+                        <a href="{{ route('evaluation_a.show') }}" type="button" class="btn btn-success btn-lg btn-block">
+                            Α Φάση: Έναρξη Αξιολόγησης
+                        </a>
+                    </p>
+                @endif
 
                 @include('pages.partials.registers')                
 
